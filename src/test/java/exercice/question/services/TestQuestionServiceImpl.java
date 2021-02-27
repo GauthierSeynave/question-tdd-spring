a4f04c22ac5cfc05aa9cd167d28414cedced405c
Gauthier Seynave <gauthier.seynave@gmail.com>
2021-02-27 20:04:52 +0100

    Configuration JUnit, Lombock, création premier test

--- src/test/java/exercice/question/services/TestQuestionServiceImpl.java
+++ src/test/java/exercice/question/services/TestQuestionServiceImpl.java
@@ -1,30 +1,37 @@
 package exercice.question.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
 
+import exercice.question.dto.NouvelleQuestion;
+import exercice.question.entities.Question;
 import exercice.question.repositories.QuestionRepository;
 import exercice.question.repositories.fake.FakeQuestionRepository;
 import exercice.question.services.impl.QuestionServiceImpl;
 
 @Testable
 public class TestQuestionServiceImpl {
-
+	
 	private QuestionRepository  repository  = new FakeQuestionRepository();
 	private QuestionService questionService = new QuestionServiceImpl(repository);
-	
-	/**
-	 * Ecrire vos tests ici.
-	 */
-	/*
-	 * Exemple de test.
-	 * @DisplayName permet de donner un nom correct a votre test. C'est le nom afficher lors du resultat des test.
-	 * Utiliser des noms qui font comprendre ce que votre test fait.
-	@Test
-	@DisplayName("Nom de votre test")
-	public void nom_du_test() {
-		Object resultat = questionService.methodeATester(param);
-		assertEquest(expected, resultat);
-	}*/
+
+	@Nested
+	@DisplayName("Exercice 1")
+	public class Exercice1 {
+		
+		@Test
+		@DisplayName("1.1 -> Quand on créer une Nouvellequestion, on récupère une Question possédant le même titre que la NouvelleQuestion")
+		public void recuperationQuestionAvecTitreIdentiqueANouvelleQuestion() {
+			NouvelleQuestion nouvelleQuestion = new NouvelleQuestion();
+			nouvelleQuestion.setQuestion("TitreDeLaQuestion");
+			
+			Question question = questionService.nouvelleQuestion(nouvelleQuestion);
+			assertEquals(nouvelleQuestion.getQuestion(), question.getTitre());
+		}
+	}
 }
